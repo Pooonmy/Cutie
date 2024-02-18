b253aa6b319067443c04e458ee9f48128ab0746c
Pisit Pankewjana <pisit.poon.45@gmail.com>
2024-02-18 18:43:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const question = document.querySelector(".question");
-const yesBtn = document.querySelector(".yesButton");
 const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yesButton");
+const noBtn = document.querySelector(".noButton");
 
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Yay, see you!";
     gif.src ="https://media.giphy.com/media/yNvmkSbnamOXEfnYxY/giphy.gif";
+    yesBtn.style.backgroundColor = '#f9e7f7';
+    yesBtn.style.borderColor = '#f9e7f7';
+    yesBtn.style.color = '#f9e7f7';
+    noBtn.style.backgroundColor = '#f9e7f7';
+    noBtn.style.borderColor = '#f9e7f7';
+    noBtn.style.color = '#f9e7f7';
 });
